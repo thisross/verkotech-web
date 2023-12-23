70b1765b25e9648a95eee5e4dd2d5662daa2e281
Eder Rosa <ederrosa@Eders-MacBook-Air.local>
2023-12-23 00:55:07 -0300

    fix

--- src/components/AboutUs/joinUs/index.tsx
+++ src/components/AboutUs/joinUs/index.tsx
@@ -3,12 +3,12 @@ import { Forward } from 'lucide-react'
 
 export default function JoinUs() {
   return (
-    <section className="flex w-full items-center justify-center pt-20">
+    <section className="flex w-full items-center justify-center px-4 pt-20">
       <div className="mx-auto flex max-w-[1200px] flex-col items-center">
         <span className="bg-verkotech-primary mb-6 rounded-[8px] p-1 px-2 font-semibold">
           Join Us
         </span>
-        <p className="mb-8 max-w-xl text-center text-3xl text-slate-400">
+        <p className="mb-8 max-w-xl text-center text-xl text-slate-400 md:text-3xl">
           {
             " Join us on this exciting journey towards the digital future. At VERKOTECH, we don't just embrace change,"
           }
